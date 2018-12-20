dfdaa4fbf09dca36c543103825c8f1c0b514c512
Evgeny Okishev <e.okishev@lcgroup.su>
2018-12-20 22:13:06 +0700

    Задание №21: Организовать запуск тестов на сервере непрерывной интеграции

--- addressbook-web-tests/src/test/java/ru/mytest/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/mytest/addressbook/appmanager/ApplicationManager.java
@@ -1,15 +1,19 @@
 package ru.mytest.addressbook.appmanager;
 
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.BrowserType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.URL;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
@@ -39,12 +43,19 @@ public class ApplicationManager {
 
     dbHelper = new DbHelper();
 
-    if (browser.equals(BrowserType.FIREFOX)) {
-      driver = new FirefoxDriver();
-    } else if (browser.equals(BrowserType.CHROME)) {
-      driver = new ChromeDriver();
-    } else if (browser.equals(BrowserType.IE)) {
-      driver = new InternetExplorerDriver();
+    if ("".equals(properties.getProperty("selenium.server"))) { //for remote start of browser on selenium server, stand-alone
+      if (browser.equals(BrowserType.FIREFOX)) {
+        driver = new FirefoxDriver();
+      } else if (browser.equals(BrowserType.CHROME)) {
+        driver = new ChromeDriver();
+      } else if (browser.equals(BrowserType.IE)) {
+        driver = new InternetExplorerDriver();
+      }
+    } else {
+      DesiredCapabilities capabilities = new DesiredCapabilities();
+      capabilities.setBrowserName(browser);
+      capabilities.setPlatform(Platform.fromString(System.getProperty("platform", "win10")));
+      driver = new RemoteWebDriver(new URL(properties.getProperty("selenium.server")), capabilities);
     }
     baseUrl = "https://www.katalon.com/";
     driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
